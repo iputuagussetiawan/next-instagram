59fde015a8e10727433563877f70cbc85e41f260
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-11-13 21:15:19 +0800

    add next auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
 
-export default function Home() {
+import { auth, signIn } from "@/auth"
+export default async function Home() {
+  const session = await auth();
   return (
-    <div>Hellow World</div>
+    <div className="">
+      {/* {JSON.stringify(session)} */}
+      <h1>Login Here</h1>
+      {session &&(
+          <form
+            action={async () => {
+              "use server"
+              await signIn("google")
+            }}
+          >
+            <button className="border px-4 py-2 bg-red-500 text-white" type="submit">Logout</button>
+          </form>
+      )}
+      {!session && (
+        <form
+          action={async () => {
+            "use server"
+            await signIn("google")
+          }}
+        >
+          <button className="border px-4 py-2 bg-blue-500 text-white" type="submit">Signin with Google</button>
+        </form>
+      )}
+    </div>
   );
 }
