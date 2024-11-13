81e5defba41eeb60a82733e94dcbcbceb0ed5a14
I Putu Agus Setiawan <agussetiawaniputu@gmail.com>
2024-11-14 05:59:58 +0800

    page profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 
-import { auth, signIn } from "@/auth"
+import { auth, signIn, signOut } from "@/auth"
 export default async function Home() {
   const session = await auth();
   return (
@@ -10,7 +10,7 @@ export default async function Home() {
           <form
             action={async () => {
               "use server"
-              await signIn("google")
+              await signOut()
             }}
           >
             <button className="border px-4 py-2 bg-red-500 text-white" type="submit">Logout</button>
